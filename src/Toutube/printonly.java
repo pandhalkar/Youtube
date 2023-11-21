d6d4a7768736f77819954abea15acb1efdda0b88
lru3825 <lru3825@PG027YYZ.mshome.net>
2023-11-21 16:54:37 +0530

    commit to the branch - printonly

---
+++ src/Toutube/printonly.java
@@ -0,0 +1,11 @@
+package Toutube;
+
+public class printonly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("FOr github repository");
+
+	}
+
+}
