03510280e512fcf2be4c0ebe8ec8eb36315960f8
lru3825 <lru3825@PG027YYZ.mshome.net>
2023-11-21 16:59:50 +0530

    slight chnage

--- src/Toutube/printonly.java
+++ src/Toutube/printonly.java
@@ -4,7 +4,7 @@ public class printonly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("FOr github repository");
+		System.out.println("FOr github repository for now");
 
 	}
 
